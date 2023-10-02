7b649a93a065d4695eb229cc556e06a559c729dc
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-10-02 21:01:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
+// write your codes here//
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    // Split the tutorial name into words
+    const words = tutorial.split(' ');
+
+    // Capitalize the first letter of each word
+    const titleCasedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+
+    // Join the title-cased words back together
+    return titleCasedWords.join(' ');
+  });
 }
+
+const titleCasedTutorials = titleCased();
+console.log(titleCasedTutorials);
